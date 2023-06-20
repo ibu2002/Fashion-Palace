69eb5de4d2d304e6ecd4aa8aff03002c09d60d07
ibu@2002 <ibu2701@outlook.com>
2023-06-20 15:59:18 +0530

    Initial Commit of my FashionPalace Website

---
+++ javaScript/script.js
@@ -0,0 +1,22 @@
+let a,b,c,d,e,f,g;
+a = 10;
+b = 20;
+c = 30;
+d = a + b + c;
+e = a - b - c;
+f = a * b * c;
+g = (a / b);
+
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+for (let i = 1; i <= 6 ; i++) {
+    console.log(" ")
+    for (let j = 6; j < i; j--) {
+        console.log(j);
+        
+    }
+    
+}
